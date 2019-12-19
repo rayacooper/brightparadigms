4fced4b334110ce4c832467f78751ae1a53f2ff6
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-12-19 14:56:47 -0700

    About Page Basics

--- src/Components/About/About Main/About.js
+++ src/Components/About/About Main/About.js
@@ -14,10 +14,39 @@ const About = () => {
                 <h5>Temperature</h5>
             </div>
 
-            <div className="AboutForrest">
-                <h2>Oh hey what's up</h2>
-                <p>Lorom ipsum dolor est</p>
+            <div className="AboutSections" id='AboutForrest'>
+                <h2>About Forrest</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
+
+            <div className="AboutSections" id='AboutLighting'>
+                <h2>Bring your lighting into the modern age</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div className="AboutSections" id='AboutShading'>
+                <h2>Bring your lighting into the modern age</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
         </div>
     )
 }
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+import './Contact.css'
+
 const Contact = () => {
     return(
-        <div>Contact</div>
+        <div className='ContactMain'>
+
+            <div className="ContactInner">
+                <p><b>CONNECT</b></p>
+                <br />
+                <p>FACEBOOK</p>
+                <p>INSTAGRAM</p>
+                <p>TWITTER</p> 
+            </div>
+
+            <div className="ContactInner">
+                <p><b>CONTACT</b></p>
+                <br />
+                <p>1970 NORTH 45 WEST</p>
+                <p>OREM UT 84663</p>
+                <br />
+                <p>000 000 0000</p>
+                <a href='mailto: workingemail@brightparadigms.com' target='blank'><p>WORKINGEMAIL@BRIGHTPARADIGMS.COM</p></a>
+                <br />
+            </div>
+
+            <p></p>
+            
+        </div>
     )
 }
 
