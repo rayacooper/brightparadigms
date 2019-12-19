e07c6eb4c2d88d0de7ae865659a8009facf10479
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-12-19 09:01:10 -0700

    First Deploy Version

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,17 +8,21 @@ import Hamburger from './../../Assets/hamburger-menu.png'
 const NavBar = () => {
     return(
         <div className="NavBarMain">
+
             <div className="NavBarLogo">
                 <h3>BRIGHT PARADIGMS</h3>
             </div>
+
             <div className='NavBarLinks'>
                 <Link className='LinkLink' to='/'>Home</Link>
                 <Link className='LinkLink' to='/about'>About Us</Link>
                 <Link className='LinkLink' to='/contact'>Contact</Link>
             </div>
+
             <div className="NavBarHamburger">
                 <img src={Hamburger} alt='Menu'/>
             </div>
+            
         </div>
     )
 }
