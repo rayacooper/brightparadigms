6e7a33558f825ed5b1033f46ebb7fc1384b2c4dd
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-01-02 13:00:04 -0700

    Fixed 'About' Page

--- src/Components/About/About Main/About.js
+++ src/Components/About/About Main/About.js
@@ -56,7 +56,7 @@ const About = () => {
                     </div>
 
                     <div className={temperatureVisible ? "AboutMiniSections" : "AboutHidden"} id='AboutTemperature'>
-                        <h1 onClick={() => updateTemperatureVisible}>Temperature</h1>
+                        <h1 onClick={() => updateTemperatureVisible(!temperatureVisible)}>Temperature</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
