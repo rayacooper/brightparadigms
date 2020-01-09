d2c9d7545e55ea2dc7491974dad4d3ddea4b2db1
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-01-09 13:49:47 -0700

    Update main page and news page appearance

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,12 +2,59 @@ import React from 'react';
 
 import './HomePage.css'
 
-import House from './../../Assets/Main-House.jpg'
+import House from './../../Assets/Main-House.jpg';
+import TavaciExterior from './../../Assets/Tavaci_Exterior.jpg';
+import TavaciKitchen from './../../Assets/Tavaci_Kitchen.jpg';
+import GreenInterior from './../../Assets/Green_Turtle_Interior.jpg';
 
 const HomePage = () => {
     return(
         <div className="HomePageMain">
             <img src={House} alt='Home Photography'/>
+            <div className="HomePageStories">
+                <div className='HomePageStory'>
+                    <img src={TavaciExterior} alt='Exterior'/>
+                    <span>
+                        <h3>The Tavaci House</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                            culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </span>
+                </div>
+                
+                <div className='HomePageStory'>
+                    <img src={TavaciKitchen} alt='Kitchen'/>
+                    <span>
+                        <h3>Tavaci Kitchen</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                            culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </span>
+                </div>
+
+                <div className='HomePageStory'>
+                    <img src={GreenInterior} alt='Interior'/>
+                    <span>
+                        <h3>Green Turtle Interior</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                            culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </span>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -12,24 +12,26 @@ const News = () => {
                 <p>02.03.2020</p>
                 <div className="NewsEntryInner">
                     <img src={Photo} />
-                    <p>
-                        We here at Bright Paradigms are excited for our launch.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                        culpa qui officia deserunt mollit anim id est laborum.
-                        <br /> <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </p>  
+                        <p>
+                            We here at Bright Paradigms are excited for our launch.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
                 </div>
                 
             </div>
