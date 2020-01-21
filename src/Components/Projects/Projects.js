e19d4eea455e8a33ea8f4289376d2e452bfdffe8
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-01-21 09:54:42 -0700

    Updated CSS on 'What We Do' page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,10 +10,10 @@ import SouthRim from './../../Assets/Grand-Canyon-South-Rim.jpg';
 const News = () => {
     return(
         <div className='NewsMain'>
-            <a href='https://www.vantagecontrols.com/about/press-room/news/vantage-grandcanyon-visitor-center.aspx'>
+            <a href='https://www.vantagecontrols.com/about/press-room/news/vantage-grandcanyon-visitor-center.aspx' target='blank'>
                 <div className='NewsEntry'>
                     <h1>Grand Canyon South Rim Visitor Center</h1>
-                    <p>02.03.2020</p>
+                    <p>12.06.2017</p>
                     <img src={SouthRim} alt='Project'/>
                 </div>
             </a>
