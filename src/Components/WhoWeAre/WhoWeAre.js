a4467725ed781e54ce66e3dd3166a8d396f64918
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-01-27 16:00:24 -0700

    Added Forrest G Photo

--- src/Components/WhoWeAre/WhoWeAre.js
+++ src/Components/WhoWeAre/WhoWeAre.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './WhoWeAre.css'
 
+import ForrestGKelsey from './../../Assets/DSC_0671-Edit.jpg';
+
 function WhoWeAre(){
     return(
         <div className="WhoMain">
             <div className='WhoInner'>
             <h1>Forrest D Kelsey</h1>
+            <h4>Presidend and Chief Technical Officer</h4>
+                <div className='WhoRow'>
                 <img src='#' alt='Forrest D Kelsey'/>
                 <p>eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
@@ -14,11 +18,13 @@ function WhoWeAre(){
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
                         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
                         culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
             <div className='WhoInner'>
                 <h1>Forrest G Kelsey</h1>
                 <h4>Chief Executive Officer</h4>
-                <img src='#' alt='Forrest G Kelsey'/>
+                <div className="WhoRow">
+                <img src={ForrestGKelsey} alt='Forrest G Kelsey'/>
                 <p>Forrest Grant Kelsey is the son of Forrest Dix Kelsey 
                     and has grown up around Vantage products most of his life. 
                     As a teenager he became familiar with the home automation 
@@ -30,6 +36,8 @@ function WhoWeAre(){
                     providers. Forrest Grant received his Bachelor’s degree in 
                     Business Management from Utah Valley University and expects 
                     to complete his MBA in August 2020 through Utah State University. </p>
+                </div>
+                
             </div>
         </div>
     )
