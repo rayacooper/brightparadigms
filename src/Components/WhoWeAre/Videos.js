c24a67b9baec28d9cedc1af780d27d64d8514508
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-02-20 17:25:10 -0700

    Added Equinox Videos

--- src/Components/WhoWeAre/Videos.js
+++ src/Components/WhoWeAre/Videos.js
@@ -6,6 +6,10 @@ import ClimateWidget02 from './../../Assets/Climate_Widget_Overview_02.mp4';
 import DesignCenter01 from './../../Assets/Design_Center_Introducing_the_Library.mp4';
 import DesignCenter02 from './../../Assets/Design_Center_Adding_to_your_Library.mp4';
 import DesignCenter03 from './../../Assets/Design_Center_Managing_your_Library.mp4';
+import Equinox01 from './../../Assets/Equinox_Shades_Widget_01.mp4';
+import Equinox02 from './../../Assets/Equinox_Shades_Widget_02';
+import EquinoxTimer01 from './../../Assets/Equinox_Timers_Widget_01.mp4';
+import EquinoxTimer02 from './../../Assets/Equinox_Timers_Widget_02.mp4'
 import './Videos.css'
 
 const Videos = () => {
@@ -42,6 +46,34 @@ const Videos = () => {
                 {/* <source src={'#'} type="video/ogg" />> */}
             </video>
             <h1 className='VideoLabel'>Design Center - Managing your library</h1>
+            <br />
+
+            <video controls>
+                <source src={Equinox01} type="video/mp4" />>
+                {/* <source src={'#'} type="video/ogg" />> */}
+            </video>
+            <h1 className='VideoLabel'>Equinox Shades Widget 1 of 2</h1>
+            <br />
+
+            <video controls>
+                <source src={Equinox02} type="video/mp4" />>
+                {/* <source src={'#'} type="video/ogg" />> */}
+            </video>
+            <h1 className='VideoLabel'>Equinox Shades Widget 2 of 2</h1>
+            <br />
+
+            <video controls>
+                <source src={EquinoxTimer01} type="video/mp4" />>
+                {/* <source src={'#'} type="video/ogg" />> */}
+            </video>
+            <h1 className='VideoLabel'>Equinox Timer Widget 1 of 2</h1>
+            <br />
+
+            <video controls>
+                <source src={EquinoxTimer02} type="video/mp4" />>
+                {/* <source src={'#'} type="video/ogg" />> */}
+            </video>
+            <h1 className='VideoLabel'>Equinox Timer Widget 2 of 2</h1>
             <br />
         </div>
     )
